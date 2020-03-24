995f216a28d794a2df1c30b560ed358632db4a37
zgard <zfgardner@gmail.com>
2020-03-24 19:20:21 -0400

    Authentication is working and redirects to myLitterBox

--- index.js
+++ index.js
@@ -1,32 +1,17 @@
 window.addEventListener("DOMContentLoaded", event => {
     console.log("DOM fully loaded and parsed");
-  
-  
-    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
         var ui = new firebaseui.auth.AuthUI(firebase.auth());
         var uiConfig = {
             callbacks: {
               signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-                const main = document.getElementById('main');
-                main.innerHTML=`<div class="container">
-                <div class="welcome">
-                    <h1 class="greeting">Welcome</h1>
-                    <div><img src="${authResult.user.photoURL}" /></div>
-                    <div id="userInfo">${authResult.user.displayName}</div>
-                </div>
-                <div class="logout">
-                    <button class="login-page" onlick="LoginPage">Logout</button>
-                </div>
-            </div>`
-              
-                  return false;
+                  return true;
               },
               uiShown: function() {
                 document.getElementById('loader').style.display = 'none';
               }
             },
             signInFlow: 'popup',
-           
+            signInSuccessUrl: 'myLitterBox.html',
             signInOptions: [
               firebase.auth.GoogleAuthProvider.PROVIDER_ID,
               firebase.auth.FacebookAuthProvider.PROVIDER_ID,
@@ -37,5 +22,51 @@ window.addEventListener("DOMContentLoaded", event => {
             privacyPolicyUrl: '<your-privacy-policy-url>'
           };
     ui.start('#firebaseui-auth-container', uiConfig);
-    
+  });
+
+  //Code for auth flow
+  firebase.auth().onAuthStateChanged(function(user) {
+    const signInButton = document.getElementById('sign-in');
+    if (user) {
+      // User is signed in.
+      if(signInButton){
+        signInButton.innerHTML = 'Sign Out';
+        signInButton.addEventListener('click', function(){
+          firebase.auth().signOut().then(() =>{
+              this.innerHTML = 'Sign In'
+          })
+        })
+      }
+      // Once this fires it means we have successfully found a user and we should be on our myLitterBox.html page now.
+      //We will target the container inside of our dashboard using this line.
+      var dashboardContainer = document.getElementById('myLitterBox');
+      if (dashboardContainer) {
+        // If we made it here we are both on the correct HTML paged and we found the container that is inside of our dashboard.html
+        var userHTML = `<h1>Hello, ${user.displayName}</h1>
+                <p>Welcome to your litter box!</p>
+        `
+        dashboardContainer.innerHTML = userHTML;
+      }
+    } else {
+      // No user is signed in. So we add an event listener to the sign in button to open our sign in modal.
+      if(signInButton){
+        signInButton.addEventListener('click', function(event){
+          const modal = document.getElementById('modal');
+          modal.style.display = 'flex'
+        })
+      }
+    }
+
+
+
+
+  // Code to open & close sign in Modal in HOMEPAGE.
+  const modal = document.getElementById('modal');
+  if(modal){
+    modal.addEventListener('click', function(event){
+      if(event.target == modal || document.getElementById('close')){
+        modal.style.display = 'none'
+      }
+    })
+  }
   });
